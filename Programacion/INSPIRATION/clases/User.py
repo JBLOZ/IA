9263f7fed19b23793909979b7848a47e2b818132
JBLOZ <jordiblloz@gmail.com>
2024-04-09 23:52:35 +0200

    23520904

--- Programacion/INSPIRATION/clases/User.py
+++ Programacion/INSPIRATION/clases/User.py
@@ -1,6 +1,34 @@
 
-
 class User:
     def __init__(self):
         pass
+
+class Tweet(User):
+    def __init__(self, user, tweet):
+        pass
+
+class Admin(User):
+    def __init__(self, user):
+        pass
+
+class Config:
+    def __init__(self):
+        pass
+
+class Database:
+    def __init__(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
     
