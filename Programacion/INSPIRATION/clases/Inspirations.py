60a8bf23fbad3725eef441368c781dbe093f33af
JBLOZ <jordiblloz@gmail.com>
2024-04-24 17:32:39 +0200

    17322024

--- Programacion/INSPIRATION/clases/Inspirations.py
+++ Programacion/INSPIRATION/clases/Inspirations.py
@@ -1,10 +1,54 @@
 import User
 import datetime
+import pickle
 class Inspiration:
-    def __init__(self, user:User.User, texto:str):
+    def __init__(self, user:User, texto:str):
         self.user = user
         self.texto = texto
         self.fecha_y_hora = datetime.datetime.now()
 
         self.likes = 0
         self.comentarios = []
+
+
+
+def lector_inspirations():
+    try:
+        with open("inspirations.pickle", "rb") as file:
+            lista_inspirations = pickle.load(file)
+    except EOFError:
+        lista_inspirations = []
+
+    return lista_inspirations
+
+def guardar_inspirations(inspiration:Inspiration):
+    lista_inspirations = lector_inspirations()
+    lista_inspirations.append(inspiration)
+
+    with open("inspirations.pickle", "wb") as file:
+        pickle.dump(lista_inspirations, file)
+    return
+
+def mostrar_inspirations(usuario:User):
+    lista_inspirations = lector_inspirations()
+    lista_inspirations.sort(key = lambda x: x.fecha_y_hora, reverse = True)
+    for i in lista_inspirations:
+        if i.user.nickname in usuario.listaSiguiendo:
+            print('@',i.user.nickname)
+            print(i.texto)
+            print('♡',i.likes)
+            print('▱',len(i.comentarios))
+            print("\n")
+
+    return
+
+def crear_inspiration(usuario:User):
+    texto = input("Ingrese el texto de la inspiration: ")
+    inspiration = Inspiration(usuario, texto)
+    usuario += inspiration
+    guardar_inspirations(inspiration)
+    return
+
+
+
+
--- Programacion/INSPIRATION/clases/Menus.py
+++ Programacion/INSPIRATION/clases/Menus.py
@@ -1,5 +1,5 @@
 import User
-
+import Inspirations
 def menuPrincipal():
     print("Bienvenido a Inspiration")
     print("1. Iniciar sesión")
@@ -7,16 +7,18 @@ def menuPrincipal():
     print("3. Salir")
     opcion = input("Ingrese una opción: ")
     if opcion == "1":
-        User.iniciar_sesion()
+        userActivo = User.iniciar_sesion()
+        menuUsuario(userActivo)
     elif opcion == "2":
         User.crear_usuario()
+        menuPrincipal()
     elif opcion == "3":
         return
     else:
         print("Opción inválida")
         menuPrincipal()
 
-def menuUsuario():
+def menuUsuario(userActivo):
     print("1. Ver inspirations")
     print("2. Crear inspiration")
     print("3. Buscar usuarios")
@@ -24,13 +26,13 @@ def menuUsuario():
     print("5. Cerrar sesión")
     opcion = input("Ingrese una opción: ")
     if opcion == "1":
-        pass
+        Inspirations.mostrar_inspirations(userActivo)
     elif opcion == "2":
-        pass
+        Inspirations.crear_inspiration(userActivo)
     elif opcion == "3":
         pass
     elif opcion == "4":
-        pass
+        userActivo.mostrar_perfil()
     elif opcion == "5":
         return
     else:
