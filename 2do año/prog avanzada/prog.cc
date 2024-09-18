e78c305d2d8dd33a24c8ddbd1e27dcd073914855
JBLOZ <jordiblloz@gmail.com>
2024-09-18 23:56:03 +0200

    cambios pc prog

--- 2do año/prog avanzada/prog.cc
+++ 2do año/prog avanzada/prog.cc
@@ -1,15 +1,120 @@
 #include <iostream>
 using namespace std;
-#include <cstdlib>
-#include <ctime>
-int main(){
 
-    int a, b, c;
-    a = 10;
-
-    cout << a << endl;
-    srand(static_cast<unsigned  int>(time(0)));
-    a = rand() % 10;
-    cout << a << endl;
+int calculadora(int a, int b) {
+    char oper;
+    std::cin >> oper;
+    if (oper=='+')
+    {
+        return (a+b);
+    }
+    if (oper=='-')
+    {
+        return (a-b);
+    }
+    if (oper=='*')
+    {
+        return (a*b);
+    }
+    if (oper=='/')
+    {
+        return (a/b);
+    }
     return 0;
 }
+
+int menor(int a, int b)
+{
+    if (a>b)
+    {
+        return b;
+    }
+    else
+    {
+        return a;
+    }
+}
+
+void FlizzBuzz()
+{
+    for (int i=1;i <= 100; i++)
+    {
+
+        if (i % 3 == 0)
+        {
+            if (i % 5 ==0)
+            {
+                std::cout << "FlizzBuzz" << std::endl;
+            }
+            else
+            {
+                std::cout << "Flizz" << std::endl;
+            }
+        }
+        else
+        {
+            if(i % 5 == 0)
+            {
+                std::cout << "Buzz" << std::endl;
+            }
+            else
+            {
+                std::cout << i << std::endl;
+            }
+        }
+    }
+}
+void triangulo(int num)
+{
+    for (int i=0;i<=num;i++,cout << endl)
+    {
+        for (int j=0;j<i;j++)
+        {
+            cout << '*';
+        }
+    }
+}
+void trianguloReves(int num)
+{
+    for (int i=0;i<=num;i++,cout << endl)
+    {
+        for (int j=0;j<num-i;j++)
+        {
+            cout << ' ';
+        }
+        for (int j=0;j<i;j++)
+        {
+            cout << '*';
+        }
+
+    }
+}
+void trianguloVerdad(int num)
+{
+    for (int i=0;i<=num;i++,cout << endl)
+    {
+        for (int j=0;j<num-i;j++)
+        {
+            cout << ' ';
+        }
+        for (int j=0;j<i;j++)
+        {
+            cout << '*';
+        }
+        for (int j=0;j<i;j++)
+        {
+            cout << '*';
+        }
+
+
+    }
+}
+int main(){
+
+    trianguloVerdad(4);
+
+    return 0;
+}
+
+
+
