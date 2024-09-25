abdc5eaefe5a9902a0499979a409fee4247ddd82
JBLOZ <jordiblloz@gmail.com>
2024-09-26 00:04:15 +0200

    afrii y apt get pstree

--- 2doAnyo/sistemasDistrubuidos/ejec.c
+++ 2doAnyo/sistemasDistrubuidos/ejec.c
@@ -3,174 +3,125 @@
 #include <unistd.h>
 #include <signal.h>
 #include <sys/wait.h>
-#include <string.h>
 
 pid_t pid_A, pid_B, pid_X, pid_Y, pid_Z;
 pid_t pid_primer_progenitor, pid_segundo_progenitor;
-char proceso_objetivo;
-int tiempo_espera;
+char *proceso_objetivo;
 
-void manejador_orden(int sig)
-{
-    if (sig == SIGUSR1)
-    {
-        if (proceso_objetivo == 'A' || proceso_objetivo == 'B')
-        {
-            printf("Soy el proceso %c con pid %d, he recibido la señal.\n", proceso_objetivo, getpid());
-            execlp("pstree", "pstree", NULL);
-        }
-        else if (proceso_objetivo == 'X' || proceso_objetivo == 'Y')
-        {
-            printf("Soy el proceso %c con pid %d, he recibido la señal.\n", proceso_objetivo, getpid());
-            execlp("ls", "ls", "-al", NULL);
+void ejecutar_orden(int signo) {
+    if (signo == SIGUSR1) {
+        if (strcmp(proceso_objetivo, "A") == 0 || strcmp(proceso_objetivo, "B") == 0) {
+            printf("Soy el proceso %s con pid %d, he recibido la señal.\n", proceso_objetivo, getpid());
+            system("pstree");
+        } else if (strcmp(proceso_objetivo, "X") == 0 || strcmp(proceso_objetivo, "Y") == 0) {
+            printf("Soy el proceso %s con pid %d, he recibido la señal.\n", proceso_objetivo, getpid());
+            system("ls");
         }
     }
 }
 
-
-void manejador_alarma(int sig)
-{
-    // Este manejador será llamado cuando el temporizador (alarm) expire
-    printf("Soy el proceso Z: mi pid es %d. Mi padre es %d. Mi abuelo es %d. Mi bisabuelo es %d\n", getpid(), getppid(), pid_segundo_progenitor, pid_primer_progenitor);
-    // Enviar señal al proceso correspondiente
-    if (proceso_objetivo == 'A')
-    {
-        kill(pid_A, SIGUSR1);
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Uso: %s <proceso> <tiempo>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
-    else if (proceso_objetivo == 'B')
-    {
-        kill(pid_B, SIGUSR1);
-    }
-    else if (proceso_objetivo == 'X')
-    {
-        kill(pid_X, SIGUSR1);
-    }
-    else if (proceso_objetivo == 'Y')
-    {
-        kill(pid_Y, SIGUSR1);
-    }
-}
 
-void esperar_procesos(pid_t *pids, int num_pids)
-{
-    for (int i = 0; i < num_pids; i++)
-    {
-        waitpid(pids[i], NULL, 0);
-    }
-}
-
-
-int main (int argc, char *argv[])
-{
-
-    if (argc != 3)
-        {
-            fprintf(stderr, "Uso: %s <proceso objetivo (A/B/X/Y)> <tiempo en segundos>\n", argv[0]);
-            exit(EXIT_FAILURE);
-        }
-
-        proceso_objetivo = argv[1][0];  // A, B, X o Y
-        tiempo_espera = atoi(argv[2]);
+    proceso_objetivo = argv[1];
+    int tiempo = atoi(argv[2]);
 
     pid_primer_progenitor = getpid();
-    printf("Soy el proceso ejec: mi pid es %d\n", getpid());
+    printf("Soy el proceso ejec: mi pid es %d\n", pid_primer_progenitor);
 
-    // Crear proceso A
     pid_A = fork();
-    if (pid_A == -1)
-    {
+    if (pid_A == -1) {
         perror("Error en fork");
         exit(EXIT_FAILURE);
     }
 
-    // Proceso A
-    if (pid_A == 0)
-    {
+    if (pid_A == 0) {
         pid_segundo_progenitor = getpid();
         printf("Soy el proceso A: mi pid es %d. Mi padre es %d\n", getpid(), getppid());
 
-        // Crear proceso B
         pid_B = fork();
-        if (pid_B == -1)
-        {
+        if (pid_B == -1) {
             perror("Error en fork");
             exit(EXIT_FAILURE);
         }
 
-        // Proceso B
-        if (pid_B == 0)
-        {
+        if (pid_B == 0) {
             printf("Soy el proceso B: mi pid es %d. Mi padre es %d. Mi abuelo es %d\n", getpid(), getppid(), pid_primer_progenitor);
 
-            // Crear proceso X
             pid_X = fork();
-            if (pid_X == -1)
-            {
+            if (pid_X == -1) {
                 perror("Error en fork");
                 exit(EXIT_FAILURE);
             }
 
-            // Proceso X
-            if (pid_X == 0)
-            {
-                printf("Soy el proceso X: mi pid es %d. Mi padre es %d. Mi abuelo es %d. Y mi bisabuelo es %d\n", getpid(), getppid(),pid_segundo_progenitor, pid_primer_progenitor);
-                pause();
-            }
-            else
-            {
-                // Crear proceso Y
+            if (pid_X == 0) {
+                printf("Soy el proceso X: mi pid es %d. Mi padre es %d. Mi abuelo es %d. Mi bisabuelo es %d\n", getpid(), getppid(), pid_segundo_progenitor, pid_primer_progenitor);
+                if (strcmp(proceso_objetivo, "X") == 0) {
+                    signal(SIGUSR1, ejecutar_orden);  // Asocia la señal a la función
+                }
+                pause();  // Espera a la señal
+            } else {
                 pid_Y = fork();
-                if (pid_Y == -1)
-                {
+                if (pid_Y == -1) {
                     perror("Error en fork");
                     exit(EXIT_FAILURE);
                 }
 
-                // Proceso Y
-                if (pid_Y == 0)
-                {
-                    printf("Soy el proceso Y: mi pid es %d. Mi padre es %d. Mi abuelo es %d. Y mi bisabuelo es %d\n", getpid(), getppid(),pid_segundo_progenitor, pid_primer_progenitor);
-                    pause();
-                }
-                else
-                {
+                if (pid_Y == 0) {
+                    printf("Soy el proceso Y: mi pid es %d. Mi padre es %d. Mi abuelo es %d. Mi bisabuelo es %d\n", getpid(), getppid(), pid_segundo_progenitor, pid_primer_progenitor);
+                    if (strcmp(proceso_objetivo, "Y") == 0) {
+                        signal(SIGUSR1, ejecutar_orden);  // Asocia la señal a la función
+                    }
+                    pause();  // Espera a la señal
+                } else {
                     pid_Z = fork();
-                    if (pid_Z == -1)
-                    {
+                    if (pid_Z == -1) {
                         perror("Error en fork");
                         exit(EXIT_FAILURE);
                     }
+
                     if (pid_Z == 0) {
-                        signal(SIGALRM, manejador_alarma);  // Asignar manejador de alarma a Z
-                        alarm(tiempo_espera);  // Iniciar temporizador
-                        pause();  // Esperar a que la alarma se dispare
+                        printf("Soy el proceso Z: mi pid es %d. Mi padre es %d. Mi abuelo es %d. Mi bisabuelo es %d\n", getpid(), getppid(), pid_segundo_progenitor, pid_primer_progenitor);
+                        sleep(tiempo);
+
+                        // Enviar la señal SIGUSR1 al proceso objetivo
+                        if (strcmp(proceso_objetivo, "A") == 0) {
+                            kill(pid_A, SIGUSR1);
+                        } else if (strcmp(proceso_objetivo, "B") == 0) {
+                            kill(pid_B, SIGUSR1);
+                        } else if (strcmp(proceso_objetivo, "X") == 0) {
+                            kill(pid_X, SIGUSR1);
+                        } else if (strcmp(proceso_objetivo, "Y") == 0) {
+                            kill(pid_Y, SIGUSR1);
+                        } else {
+                            printf("Proceso objetivo no válido\n");
+                        }
+
                         printf("Soy Z (%d) y muero\n", getpid());
-                        kill(pid_Z, 9);
-                    }
-                    else
-                    {
-                        // Esperar a que todos los procesos terminen en orden
-                        pid_t pids[] = {pid_Z, pid_Y, pid_X, pid_B};
-                        esperar_procesos(pids, 4);
+                    } else {
+                        if (strcmp(proceso_objetivo, "B") == 0) {
+                            signal(SIGUSR1, ejecutar_orden);  // Asocia la señal a la función
+                        }
+                        wait(NULL);
+                        printf("Soy Y (%d) y muero\n", pid_Y);
+                        printf("Soy X (%d) y muero\n", pid_X);
                         printf("Soy B (%d) y muero\n", pid_B);
-                        kill(pid_B, 9);
                     }
                 }
             }
+        } else {
+            if (strcmp(proceso_objetivo, "A") == 0) {
+                signal(SIGUSR1, ejecutar_orden);  // Asocia la señal a la función
+            }
+            wait(NULL);
+            printf("Soy A (%d) y muero\n", getpid());
         }
-        else
-        {
-            waitpid(pid_B, NULL, 0);  // Esperar a B
-            printf("Soy A (%d) y muero\n", pid_A);
-            kill(pid_A, 9);
-        }
-    }
-    else
-    {
-        waitpid(pid_A, NULL, 0);  // Esperar a A
+    } else {
+        wait(NULL);
         printf("Soy ejec (%d) y muero\n", pid_primer_progenitor);
-        kill(pid_primer_progenitor, 9);
+        return 0;
     }
-
-    return 0;
 }
