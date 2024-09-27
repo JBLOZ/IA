01a90118d5ea1eacac2c0763e6eea6a3cc83cc65
JBLOZ <jordiblloz@gmail.com>
2024-09-27 13:48:33 +0200

    prog y python.12

--- 2doAnyo/progAvanzada/Ejemplo.cc
+++ 2doAnyo/progAvanzada/Ejemplo.cc
@@ -1,10 +1,20 @@
 #include <iostream>
 #include "Fecha.h"
+#include <string>
 using namespace std;
+
 
 int main()
 {
-    Fecha f;
+    Fecha fecha1 = Fecha();
     cout << "todo bien" << endl;
+    Fecha fecha3 = Fecha(4,11,2020);
+
+    cout << fecha1.aCadena(true,true) << endl;
+
+    cout << fecha3.aCadena(true, true);
+
+
+    
     return 0;
 }
--- 2doAnyo/progAvanzada/Fecha.cc
+++ 2doAnyo/progAvanzada/Fecha.cc
@@ -1,10 +1,86 @@
 #include "Fecha.h"
+#include <iostream>
+#include "FechaUtils.h"
+using namespace std;
+
+
 
 Fecha::Fecha()
-{
-
+{   
+    cout << "clase fecha construida" << endl;
+    dia = 1;
+    mes = 1;
+    anyo = 1900;
 }
 
 Fecha::~Fecha()
 {
+    cout << "clase fecha destruida" << endl;
+    dia = 1;
+    mes = 1;
+    anyo = 1900;
+}
+
+Fecha::Fecha(int dia, int mes, int anyo)
+{
+    cout << "clase fecha construida con parametros" << endl;
+    if (FechaUtils::compruebaFecha(dia, mes, anyo))
+    {
+        cout << dia << mes << anyo;
+    } 
+    else
+    {   
+        cout << "fecha incorrecta" << endl;
+        *this = Fecha();
+    }
+}
+
+Fecha::Fecha(const Fecha& other)
+{
+    cout << "clase fecha copiada" << endl;
+    this->dia = other.dia;
+    this->mes = other.mes;
+    this->anyo = other.anyo;
+}
+
+Fecha& Fecha::operator=(const Fecha &other)
+{
+    if (this != &other) {
+        this->dia = other.dia;
+        this->mes = other.mes;
+        this->anyo = other.anyo;
+    }
+    return *this;
+}
+
+int Fecha::getDia() const
+{
+    return dia;
+}
+
+int Fecha::getMes() const
+{
+    return mes;
+}
+
+int Fecha::getAnyo() const
+{
+    return anyo;
+}
+
+bool Fecha::setDia(int)
+{
+    return true;
+}
+
+string Fecha::aCadena(bool larga, bool conDia) const
+{
+    string cadena = "";
+    if (conDia)
+    {
+        cadena += to_string(dia);
+    }  
+    cadena += "/" + to_string(mes) + "/" + to_string(anyo);
+
+    cout << cadena;
 }
