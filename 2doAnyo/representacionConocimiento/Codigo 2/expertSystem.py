55b7e924e6538236b13347eb33ca7e795ac2a2c2
JBLOZ <jordiblloz@gmail.com>
2024-09-30 15:25:31 +0200

    representacionConocimiento

--- 2doAnyo/representacionConocimiento/Codigo 2/expertSystem.py
+++ 2doAnyo/representacionConocimiento/Codigo 2/expertSystem.py
@@ -1,4 +1,15 @@
-import math
+'''
+ Sistema Experto para el guiado de un robot
+ Esta clase contendrá el código creado por los alumnos de RyRDC para el control 
+ y guiado de un robot móvil sobre un plano cartesiano pasando por un punto inicial
+ y siguiendo una línea recta hasta un punto final
+
+ Creado por: Diego Viejo
+ el 26/09/2024
+ Modificado por: 
+
+'''
+
 from segmento import *
 
 class ExpertSystem:
@@ -8,64 +19,27 @@ class ExpertSystem:
 
     # función setObjetivo
     #   Especifica un segmento como objetivo para el recorrido del robot
-    #   Este método NO debería ser modificado
     def setObjetivo(self, segmento):
         self.objetivoAlcanzado = False
         self.segmentoObjetivo = segmento
 
-    # función tomarDecision
+
+    # función tomarDecision. 
     #   Recibe una tupla de 3 valores con la pose del robot y un objeto
     #   de clase Segmento con la información del segmento a seguir
-    #
+    #   
     #   Devuelve una tupla con la velocidad lineal y angular que se
     #   quiere dar al robot
     def tomarDecision(self, poseRobot):
-        # Asumimos que poseRobot es una tupla (x, y, theta)
-        if self.segmentoObjetivo is None:
-            return (0, 0)
+        # código del sistema experto. A completar por la alumna o alumno
 
-        # Obtener la posición del robot
-        x_r, y_r, theta_r = poseRobot
-
-        # Obtener el punto final del segmento
-        x_objetivo, y_objetivo = self.segmentoObjetivo.getPuntoFinal()
-
-        # Distancia entre el robot y el objetivo
-        distancia_objetivo = ((x_objetivo - x_r) ** 2 + (y_objetivo - y_r) ** 2) ** 0.5
-
-        # Definir un umbral de distancia para considerar que el objetivo ha sido alcanzado
-        umbral_alcance = 0.1  # Se puede ajustar según precisión deseada
-
-        if distancia_objetivo < umbral_alcance:
-            self.objetivoAlcanzado = True
-            return (0, 0)
-
-        # Calcular el ángulo deseado
-        angulo_deseado = math.atan2(y_objetivo - y_r, x_objetivo - x_r)
-
-        # Diferencia de ángulo
-        error_angulo = angulo_deseado - theta_r
-
-        # Normalizar el error de ángulo entre -pi y pi
-        while error_angulo > math.pi:
-            error_angulo -= 2 * math.pi
-        while error_angulo < -math.pi:
-            error_angulo += 2 * math.pi
-
-        # Constantes de control (se pueden ajustar)
-        Kp_lineal = 1.0  # Ganancia proporcional para la velocidad lineal
-        Kp_angular = 2.0  # Ganancia proporcional para la velocidad angular
-
-        # Velocidad lineal basada en la distancia al objetivo
-        velocidad_lineal = Kp_lineal * distancia_objetivo
-
-        # Velocidad angular basada en el error de ángulo
-        velocidad_angular = Kp_angular * error_angulo
-
-        return (velocidad_lineal, velocidad_angular)
-
-    # función esObjetivoAlcanzado
-    #   Devuelve True cuando el punto final del objetivo ha sido alcanzado.
+        return (3, 0.25)
+    
+    # función esObjetivoAlcanzado 
+    #   Devuelve True cuando el punto final del objetivo ha sido alcanzado. 
+    #   Es responsabilidad de la alumna o alumno cambiar el valor de la 
+    #   variable objetivoAlcanzado cuando se detecte que el robot ha llegado 
+    #   a su objetivo. Esto se llevará a cabo en el método tomarDecision
     #   Este método NO debería ser modificado
     def esObjetivoAlcanzado(self):
         return self.objetivoAlcanzado
--- 2doAnyo/representacionConocimiento/Codigo 2/segmento.py
+++ 2doAnyo/representacionConocimiento/Codigo 2/segmento.py
@@ -5,15 +5,11 @@
  ' No puede ser modificado por los alumnos
 '''
 
-import pygame
-
-RADIUS = 8
 
 class segmento:
-    def __init__(self, sizeY):
+    def __init__(self):
         self.pInicio = (0, 0)
         self.pFin = (0, 0)
-        self.sizeY = sizeY
 
     def setInicio(self, inicio):
         self.pInicio = inicio
@@ -21,17 +17,10 @@ class segmento:
     def setFin(self, fin):
         self.pFin = fin
 
-    def drawSegment(self, screen):
-        pInicio = (self.pInicio[0]*10.0, self.sizeY-self.pInicio[1]*10.0)
-        pFin = (self.pFin[0]*10.0, self.sizeY-self.pFin[1]*10.0)
-        pygame.draw.line(screen, "darkgray", pInicio, pFin, 5)
-        pygame.draw.circle(screen, "green", pInicio, RADIUS)
-        pygame.draw.circle(screen, "red", pFin, RADIUS)
-
     # Obtiene el punto de inicio del segmento
     def getInicio(self):
         return self.pInicio
     
     # Obtiene el punto final del segmento
     def getFin(self):
-        return self.getFin
+        return self.pFin
