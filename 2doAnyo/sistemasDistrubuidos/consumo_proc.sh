fa8e5b537f14dc9ad7ae4076b16c59f7b3c13338
JBLOZ <jordiblloz@gmail.com>
2024-10-07 19:13:50 +0200

    sistemas hecho

--- 2doAnyo/sistemasDistrubuidos/consumo_proc.sh
+++ 2doAnyo/sistemasDistrubuidos/consumo_proc.sh
@@ -1,5 +1,9 @@
+# Solicita al usuario el número de procesos que desea ver
 printf "Indica los procesos que quieras ver: "
+
+# Lee el valor ingresado por el usuario y lo guarda en la variable N
 read N
 
-
+# Lista todos los procesos del sistema, ordenados por uso de memoria (de mayor a menor)
+# Luego, selecciona las primeras N+1 líneas para incluir los encabezados
 ps -aux --sort=-%mem | head -n $(($N+1))
