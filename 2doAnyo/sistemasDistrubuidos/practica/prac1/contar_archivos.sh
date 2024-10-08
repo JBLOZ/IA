c20bdfae47a3b8855664cf59880f3f52867a935d
JBLOZ <jordiblloz@gmail.com>
2024-10-08 22:47:29 +0200

    sist

--- 2doAnyo/sistemasDistrubuidos/practica/prac1/contar_archivos.sh
+++ 2doAnyo/sistemasDistrubuidos/practica/prac1/contar_archivos.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+    ruta="."
+else
+    ruta="$1"
+fi
+
 # Contar el número de archivos en el directorio actual
-num_archivos=$(find . -type f | wc -l | tr -d '[:space:]')
+num_archivos=$(find "$ruta" -type f | wc -l | tr -d '[:space:]')
 
 # Contar el número de directorios en el directorio actual
-num_directorios=$(find . -type d | wc -l | tr -d '[:space:]')
+num_directorios=$(find "$ruta" -type d | wc -l | tr -d '[:space:]')
 
 # Mostrar los resultados
 printf "Número de archivos: %d\n" "$num_archivos"
