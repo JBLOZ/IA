b8304ed1e69b03bf80ffe071952a3f16c999aec8
jordi <jordiblloz@gmail.com>
2025-04-03 13:01:47 +0200

    curriculum

--- 2doAnyo/2doCuatri/computacion_de_alto_rendimiento/practicas/practica8/ejercicio3.c
+++ 2doAnyo/2doCuatri/computacion_de_alto_rendimiento/practicas/practica8/ejercicio3.c
@@ -1,15 +1,13 @@
 #include <mpi.h>
 #include <stdio.h>
-
 int main(int argc, char** argv) {
-    MPI_Init(&argc, &argv);
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int dato;
-    if (rank == 2) dato = 99; // El root inicializa el dato
-    MPI_Bcast(&dato, 1, MPI_INT, 2, MPI_COMM_WORLD); // Difusión a todos
-    dato += 1;
-    printf("Proceso %d recibió: %d\n", rank, dato);
-    MPI_Finalize();
-    return 0;
+ MPI_Init(&argc, &argv);
+ int rank;
+ MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+ int dato;
+ if (rank == 0) dato = 99; // El root inicializa el dato
+ MPI_Bcast(&dato, 1, MPI_INT, 0, MPI_COMM_WORLD); // Difusión a todos
+ printf("Proceso %d recibió: %d\n", rank, dato);
+ MPI_Finalize();
+ return 0;
 }
